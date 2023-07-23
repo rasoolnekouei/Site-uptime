683ba3e4e47958277e368f4f1220592366c15a7e
rasool <rasoolnekouei@gmail.com>
2023-07-23 19:31:30 +0330

    New Project

---
+++ Test Uptime/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace Test_Uptime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label11 = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(273, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 30);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(273, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(260, 30);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(273, 146);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(260, 30);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Site Address :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(231, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Time Request (Second) :";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Your Phone:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(273, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 43);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start Monitoring";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Gold;
+            this.label4.Location = new System.Drawing.Point(900, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 32);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Wating";
+            this.label4.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(712, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 32);
+            this.label5.TabIndex = 8;
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(712, 68);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(158, 32);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Your Site :";
+            this.label6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.Location = new System.Drawing.Point(680, 114);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 32);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "1";
+            this.label7.Visible = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 60000;
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.Navy;
+            this.label11.Location = new System.Drawing.Point(680, 343);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(116, 32);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "label11";
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 60000;
+            this.timer3.Tick += new System.EventHandler(this.Timer3_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(273, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(260, 43);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Show Uptime";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(273, 332);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(260, 43);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Send SMS";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label8.Location = new System.Drawing.Point(785, 271);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 32);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label9.Location = new System.Drawing.Point(613, 271);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(140, 32);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Duration:";
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 60000;
+            this.timer4.Tick += new System.EventHandler(this.Timer4_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1275, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Uptime";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Timer timer4;
+    }
+}
+
---
+++ Test Uptime/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Windows.Forms;
+using System.Net.Mail;
+
+
+
+namespace Test_Uptime
+{
+    public partial class Form1 : Form
+    {
+        //تعریف متغیر
+        string site;
+
+        double a ;
+       
+
+        
+        int Min=10;
+        int uptime=0;
+        int downtime=0;
+        string phone;
+        
+
+        bool Condition=false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            site = textBox1.Text;
+            label5.Text = "Request to : "+ site ;
+            label6.Visible = true;
+            label5.Visible = true;
+            label4.Visible = true;
+
+            
+            
+            //فعال کردن تایمر
+            timer1.Interval = 1000*(Convert.ToInt32( textBox2.Text));
+            timer2.Enabled = true;
+
+            
+
+            timer1.Enabled = true;
+
+            textBox1.ReadOnly = true;
+            textBox2.ReadOnly = true;
+            textBox3.ReadOnly = true;
+
+           phone = textBox3.Text;
+            
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            
+                
+
+            
+            var request = (HttpWebRequest)WebRequest.Create("http://" + site);
+
+            //a = (downtime * 100) / uptime;
+            //a = (100) - (a);
+            try
+            {
+                var response = (HttpWebResponse)request.GetResponse();
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    timer3.Enabled = true;
+                    timer4.Enabled = false;
+
+                    label4.Text = "is Up";
+                    if (Condition==false)
+                    {
+                        
+                        
+
+
+                        
+
+                        Condition = true;
+
+                       
+                        var message = "Website : " + " ' " + "http://" + textBox1.Text + " ' " + "  " + "is UP  " +"  "+"Uptime: "/*+ Math.Round(a, 2)*/+"%    " + "Time : " + DateTime.Now;
+                        var api = new Kavenegar.KavenegarApi("715563754B717570346856327444397139673037754F4A6E4F4D55736661504747624233384A492F77434D3D");
+                        api.Send("1000596446", phone, message);
+
+                       
+
+                       
+
+                       
+
+
+
+                    }
+
+
+                    label4.ForeColor = System.Drawing.Color.Green;
+                    if (timer2.Enabled==false)
+                    {
+                        timer2.Enabled = true;
+                    }
+
+                }
+
+        }
+            catch (Exception)
+            {
+
+                label4.Text = "is Down";
+
+                timer3.Enabled = false;
+                timer4.Enabled = true;
+
+                if (Condition==true)
+                {
+
+                    Condition = false;
+
+                    
+
+                    var receptor = "09028932071";
+                    var message = "Website : " + " ' " + "http://" + textBox1.Text + " ' " + "  " + "is DOWN !  " + "Time : " + DateTime.Now;
+                    var api = new Kavenegar.KavenegarApi("715563754B717570346856327444397139673037754F4A6E4F4D55736661504747624233384A492F77434D3D");
+                    api.Send("1000596446", receptor, message);
+
+
+                    
+
+                }
+                label4.ForeColor = System.Drawing.Color.Red;
+
+               
+
+                
+
+                
+                
+               
+
+                
+            }
+
+
+
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            Min++;
+            label8.Text ="Mins" + Convert.ToString(Min) ;
+        }
+
+        private void Timer3_Tick(object sender, EventArgs e)
+        {
+
+            uptime++;
+            
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            
+            a = (downtime * 100)/(uptime);
+            a = (100)-(a) ;
+
+            textBox3.Text = Convert.ToString(Math.Round(a,2));
+            label11.Text = "Uptime:  "+Convert.ToString(Math.Round(a, 2)) +"%" ;
+        }
+
+        private void Timer4_Tick(object sender, EventArgs e)
+        {
+            downtime++;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            a = (downtime * 100) / uptime;
+            a = (100) - (a);
+
+            
+
+           
+            var message = "Website : " + " ' " + "http://" + textBox1.Text + " ' " + "  " + "is UP  " + "  " + "Uptime: " + Math.Round(a, 2) + "%    " + "Time : " + DateTime.Now;
+            var api = new Kavenegar.KavenegarApi("715563754B717570346856327444397139673037754F4A6E4F4D55736661504747624233384A492F77434D3D");
+            api.Send("1000596446", phone, message);
+        }
+    }
+}
